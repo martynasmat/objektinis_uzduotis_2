d181a59c2f48f550f46dce2bbeb2072add82e0bb
martynasmat <martynasmat03@gmail.com>
2024-02-09 11:19:50 +0200

    galutinio balo skaiciavimo funkcija

--- main.cpp
+++ main.cpp
@@ -5,14 +5,15 @@
 using namespace std;
 
 struct Studentas {
-    string vardas, pavarde;
-    vector <int> nd_rez;
-    int egz_rez, n;
-    float gal_rez, avg_nd;
+    string name, last_name;
+    vector <int> hw_res;
+    int exam_res, n;
+    float final_res, avg_hw;
 };
 
 void readData(vector <Studentas> &stud);
-float average(vector <int> &rez);
+float average(vector <int> &res);
+float final(float hw, int exam);
 
 int main() {
     vector <Studentas> studentai;
@@ -22,31 +23,36 @@ int main() {
 
 void readData(vector <Studentas> &stud) {
     Studentas stud_var;
-    int nd;
+    int hw;
     cout << "Vardas: ";
-    cin >> stud_var.vardas;
+    cin >> stud_var.name;
     cout << "Pavarde: ";
-    cin >> stud_var.pavarde;
+    cin >> stud_var.last_name;
     cout << "n: ";
     cin >> stud_var.n;
     for(int i = 0; i < stud_var.n; i++) {
         cout << "Namu darbo nr. " << i + 1 << " rezultatas: ";
-        cin >> nd;
-        stud_var.nd_rez.push_back(nd);
+        cin >> hw;
+        stud_var.hw_res.push_back(hw);
     };
     cout << "Egzamino rezultatas: ";
-    cin >> stud_var.gal_rez;
-    stud_var.avg_nd = average(stud_var.nd_rez);
-    cout << stud_var.avg_nd << endl;
+    cin >> stud_var.exam_res;
+
+    stud_var.avg_hw = average(stud_var.hw_res);
+    cout << stud_var.avg_hw << endl;
     stud.push_back(stud_var);
+
+    stud_var.final_res = final(stud_var.avg_hw, stud_var.exam_res);
+    cout << " fin " << stud_var.final_res << endl;
+
     return;
 };
 
-float average(vector <int> &rez) {
-    int vec_size = rez.size();
+float average(vector <int> &res) {
+    int vec_size = res.size();
     int sum = 0;
     for(int i = 0; i < vec_size; i++) {
-        sum += rez[i];
+        sum += res[i];
     };
 
     if(vec_size > 0) {
@@ -55,3 +61,7 @@ float average(vector <int> &rez) {
         return 0.0;
     };
 };
+
+float final(float hw, int exam) {
+    return (float) 0.4 * hw + 0.6 * exam;
+}
