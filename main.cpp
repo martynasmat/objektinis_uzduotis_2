8375a227e06001371f201d5e6c6066d8b9bdbe9a
martynasmat <martynasmat03@gmail.com>
2024-02-09 11:39:20 +0200

    keliu studentu duomenu ivedimas, duomenu spausdinimas

--- main.cpp
+++ main.cpp
@@ -11,40 +11,45 @@ struct Studentas {
     float final_res, avg_hw;
 };
 
-void readData(vector <Studentas> &stud);
+void readData(vector <Studentas> &stud, int stud_num);
 float average(vector <int> &res);
 float final(float hw, int exam);
+void printData(vector <Studentas> &stud, int num);
 
 int main() {
-    vector <Studentas> studentai;
-    readData(studentai);
+    vector <Studentas> students;
+    int stud_num;
+    cout << "Moksleiviu skaicius: " << endl;
+    cin >> stud_num;
+    readData(students, stud_num);
+    printData(students, stud_num);
     return 0;
 }
 
-void readData(vector <Studentas> &stud) {
-    Studentas stud_var;
-    int hw;
-    cout << "Vardas: ";
-    cin >> stud_var.name;
-    cout << "Pavarde: ";
-    cin >> stud_var.last_name;
-    cout << "n: ";
-    cin >> stud_var.n;
-    for(int i = 0; i < stud_var.n; i++) {
-        cout << "Namu darbo nr. " << i + 1 << " rezultatas: ";
-        cin >> hw;
-        stud_var.hw_res.push_back(hw);
-    };
-    cout << "Egzamino rezultatas: ";
-    cin >> stud_var.exam_res;
+void readData(vector <Studentas> &stud, int stud_num) {
+    for(int i = 0; i < stud_num; i++) {
+        Studentas stud_var;
+        int hw;
+        cout << "Vardas: ";
+        cin >> stud_var.name;
+        cout << "Pavarde: ";
+        cin >> stud_var.last_name;
+        cout << "n: ";
+        cin >> stud_var.n;
+        for (int j = 0; j < stud_var.n; j++) {
+            cout << "Namu darbo nr. " << j + 1 << " rezultatas: ";
+            cin >> hw;
+            stud_var.hw_res.push_back(hw);
+        };
+        cout << "Egzamino rezultatas: ";
+        cin >> stud_var.exam_res;
 
-    stud_var.avg_hw = average(stud_var.hw_res);
-    cout << stud_var.avg_hw << endl;
-    stud.push_back(stud_var);
+        stud_var.avg_hw = average(stud_var.hw_res);
 
-    stud_var.final_res = final(stud_var.avg_hw, stud_var.exam_res);
-    cout << " fin " << stud_var.final_res << endl;
-
+        stud_var.final_res = final(stud_var.avg_hw, stud_var.exam_res);
+        stud.push_back(stud_var);
+        cout << endl;
+    }
     return;
 };
 
@@ -64,4 +69,10 @@ float average(vector <int> &res) {
 
 float final(float hw, int exam) {
     return (float) 0.4 * hw + 0.6 * exam;
-}
+};
+
+void printData(vector <Studentas> &stud, int num) {
+    for(int i = 0; i < num; i++) {
+        cout << stud[i].name << " " << stud[i].last_name << " " << stud[i].final_res << endl;
+    };
+};
