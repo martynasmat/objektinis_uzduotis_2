6be9eaa48bba91c366df15fdbbed9e54605ee4b2
martynasmat <martynasmat03@gmail.com>
2024-02-14 22:44:50 +0200

    skaityti is anksto nenumatyta skaiciu studentu duomenu ir pazymiu, isimciu handlingas

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <iomanip>
 #include <cmath>
 #include <algorithm>
+#include <cctype>
 
 using namespace std;
 
@@ -14,45 +15,147 @@ struct Studentas {
     float final_res, final_hw;
 };
 
-void readData(vector <Studentas> &stud, int stud_num, bool use_median);
+const int NAME_MAX_SYMBOLS = 20;
+const int NAME_MIN_SYMBOLS = 2;
+const int MIN_MARK = 2;
+const int MAX_MARK = 10;
+
+void readData(vector <Studentas> &stud, bool use_median);
 float average(vector <int> &res);
 float median(vector <int> &res);
 float final(float hw, int exam);
 void printData(vector <Studentas> &stud, int num, bool use_median);
+bool valid_mark(int input);
+bool valid_alphabet(string input);
 
 int main() {
     vector <Studentas> students;
-    int stud_num;
     bool use_median;
-    cout << "Naudoti vidurki ar mediana? (0 - vidurkis, 1 - mediana):";
-    cin >> use_median;
+    bool ivesta = false;
+    while(!ivesta) {
+        cout << "Naudoti vidurki ar mediana? (0 - vidurkis, 1 - mediana):";
+        if(cin >> use_median) {
+            if(use_median != 0 && use_median != 1) {
+                cout << "Bloga ivestis, galima ivesti tik 0 arba 1." << endl << endl;
+                cin.clear();
+                cin.ignore (std::numeric_limits<std::streamsize>::max(), '\n');
+            }else {
+                ivesta = true;
+            }
+        }else {
+            cout << "Bloga ivestis, bandykite dar karta" << endl << endl;
+            cin.clear();
+            cin.ignore (std::numeric_limits<std::streamsize>::max(), '\n');
+        };
+    };
+    cin.ignore (std::numeric_limits<std::streamsize>::max(), '\n');
     cout << endl;
-    cout << "Studentu skaicius:";
-    cin >> stud_num;
-    readData(students, stud_num, use_median);
-    printData(students, stud_num, use_median);
+    readData(students, use_median);
+    printData(students, students.size(), use_median);
     return 0;
-}
+};
 
-void readData(vector <Studentas> &stud, int stud_num, bool use_median) {
-    for(int i = 0; i < stud_num; i++) {
+bool valid_mark(int input) {
+    if (input < MIN_MARK) {
+        cout << "Bloga ivestis, pazymys turi buti lygus arba didesnis nei 2." << endl << endl;
+        return false;
+    }else if (input > MAX_MARK) {
+        cout << "Bloga ivestis, pazymys turi buti lygus arba mazenis nei 10." << endl << endl;
+        return false;
+    };
+    return true;
+};
+
+bool valid_alphabet(string input) {
+    if (input.length() <= NAME_MAX_SYMBOLS && input.length() >= NAME_MIN_SYMBOLS) {
+        for (int i = 0; i < input.length(); i++) {
+            if (!isalpha(input[i])) {
+                cout << "Bloga ivestis, galima ivesti tik raides." << endl << endl;
+                return false;
+            };
+        };
+        return true;
+    }else {
+        cout << "Bloga ivestis, min. " << NAME_MIN_SYMBOLS << ", maks. " << NAME_MAX_SYMBOLS << " simboliu." << endl << endl;
+        return false;
+    };
+};
+
+void readData(vector <Studentas> &stud, bool use_median) {
+    bool do_continue = false;
+    bool do_continue_inner = false;
+    bool ivesta;
+    bool ivesta_inner = false;
+    do{
+        string response;
         Studentas stud_var;
         int hw;
-        cout << "Vardas:";
-        cin >> stud_var.name;
-        cout << "Pavarde:";
-        cin >> stud_var.last_name;
-        cout << "n: ";
-        cin >> stud_var.n;
 
-        for (int j = 0; j < stud_var.n; j++) {
-            cout << "Namu darbo nr. " << j + 1 << " rezultatas:";
-            cin >> hw;
-            stud_var.hw_res.push_back(hw);
+        ivesta = false;
+        while(!ivesta) {
+            cout << "Vardas (maks. 20 simboliu):";
+            getline(cin, stud_var.name);
+            if(valid_alphabet(stud_var.name)) {
+                ivesta = true;
+            };
         };
-        sort(stud_var.hw_res.begin(), stud_var.hw_res.end());
-        cout << "Egzamino rezultatas:";
-        cin >> stud_var.exam_res;
+
+        ivesta = false;
+        while (!ivesta) {
+            cout << "Pavarde (maks. 20 simboliu):";
+            getline(cin, stud_var.last_name);
+            if(valid_alphabet(stud_var.last_name)) {
+                ivesta = true;
+            };
+        };
+
+        do {
+            ivesta = false;
+            cout << "Ivesti namu darbu rezultata? (y - taip, n - ne):";
+            cin >> response;
+            cout << endl;
+            while (!ivesta) {
+                if (response == "n") {
+                    do_continue_inner = false;
+                    ivesta = true;
+                } else if (response == "y") {
+                    ivesta = true;
+                    do_continue_inner = true;
+                    ivesta_inner = false;
+                    while(!ivesta_inner) {
+                        cout << "Namu darbo nr. " << stud_var.hw_res.size() + 1 << " rezultatas:";
+                        if(cin >> hw) {
+                            if (valid_mark(hw)) {
+                                stud_var.hw_res.push_back(hw);
+                                ivesta_inner = true;
+                            };
+                        }else {
+                            cout << "Bloga ivestis, galima ivesti tik sveikuosius skaicius." << endl << endl;
+                        };
+                        cin.clear();
+                        cin.ignore (std::numeric_limits<std::streamsize>::max(), '\n');
+                    };
+                } else {
+                    cout << "Bloga ivestis, bandykite dar karta." << endl << endl;
+                    ivesta = true;
+                    do_continue_inner = true;
+                };
+            };
+        }while(do_continue_inner);
+
+        ivesta = false;
+        while(!ivesta) {
+            cout << "Egzamino rezultatas:";
+            if(cin >> stud_var.exam_res) {
+                if(valid_mark(stud_var.exam_res)) {
+                    ivesta = true;
+                };
+            }else {
+                cout << "Bloga ivestis, galima ivesti tik sveikuosius skaicius." << endl << endl;
+            };
+            cin.clear();
+            cin.ignore (std::numeric_limits<std::streamsize>::max(), '\n');
+        };
 
         if(use_median) {
             stud_var.final_hw = median(stud_var.hw_res);
@@ -63,7 +166,25 @@ void readData(vector <Studentas> &stud, int stud_num, bool use_median) {
         stud_var.final_res = final(stud_var.final_hw, stud_var.exam_res);
         stud.push_back(stud_var);
         cout << endl;
-    }
+
+        ivesta = false;
+        cout << endl;
+        while(!ivesta) {
+            cout << "Ivesti dar vieno studento duomenis? (y - taip, n - ne):";
+            cin >> response;
+            if (response == "n") {
+                do_continue = false;
+                ivesta = true;
+            }else if (response == "y") {
+                do_continue = true;
+                ivesta = true;
+            }else {
+                cout << "Bloga ivestis, bandykite dar karta." << endl;
+                ivesta = false;
+            };
+        };
+        cin.ignore (std::numeric_limits<std::streamsize>::max(), '\n');
+    }while(do_continue);
 };
 
 float average(vector <int> &res) {
@@ -81,6 +202,7 @@ float average(vector <int> &res) {
 };
 
 float median(vector <int> &res) {
+    sort(res.begin(), res.end());
     int vec_size = res.size();
     int size_divided = ceil(vec_size / 2.0);
     if(vec_size > 0) {
