1a336cf32cd58764e474c3d54ed64660742f0d76
martynasmat <martynasmat03@gmail.com>
2024-03-10 11:06:51 +0200

    main f-jos pakeitimai

--- vectors.cpp
+++ vectors.cpp
@@ -12,26 +12,31 @@ int main() {
         bool generate_marks = false;
         bool generate_names = false;
         bool read_from_file = false;
+        bool generate_files = false;
         bool use_median = false;
         int menu_choice = 5;
 
-        main_menu(menu_choice, read_from_file, generate_marks, generate_names);
+        main_menu(menu_choice, read_from_file, generate_marks, generate_names, generate_files);
         output_menu(output_console);
         clear_stream();
 
-        if (read_from_file) {
-            read_data_from_file("studentai100000.txt", students);
-        }else {
-            read_data_from_console(students, use_median, generate_marks, generate_names);
-        }
+        if (generate_files) {
+            continue;
+        } else {
+            if (read_from_file) {
+                read_data_from_file("studentai100000.txt", students);
+            } else {
+                read_data_from_console(students, use_median, generate_marks, generate_names);
+            }
 
-        sort_menu(students);
+            sort_menu(students);
 
-        if (output_console) {
-            avg_median_menu(use_median);
-            print_data_choice(students, students.size(), use_median);
-        }else {
-            print_data_file(students, students.size(), use_median);
+            if (output_console) {
+                avg_median_menu(use_median);
+                print_data_choice(students, students.size(), use_median);
+            } else {
+                print_data_file(students, students.size(), use_median);
+            }
         }
     }
 }
